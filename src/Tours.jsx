813712607cc7c6bd28d76f52e963a3b106efc0de
Colin B. Chin Choy <cchinchoy@gmail.com>
2023-06-11 09:28:32 -0400

    Grab API and Render

---
+++ src/Tours.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Tour from "./Tour";
+
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>
+          our Tours
+          <div className="title-underline"></div>
+          <div className="tours">
+            {tours.map((tour) => {
+              return <Tour key={tour.id} {...tour} />;
+            })}
+          </div>
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
